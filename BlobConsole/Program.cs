76bc277b2e3407c97083007724102d58d111b490
FernandoSanabria <FernandoSanabria@github.com>
2020-07-04 10:04:41 -0500

    blobconsole.csproj configurado para leer la cadena de conexion y dependencias de microsoft requeridas agregadas

---
+++ BlobConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlobConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

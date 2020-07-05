a72a6578f58591711e8f0ade7cb8b5e36f543f98
FernandoSanabria <FernandoSanabria@github.com>
2020-07-05 09:22:19 -0500

    configuracion de conexion para proyecto con queue en el azure storage accpunt

---
+++ QueueConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
+
+namespace QueueConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                          .SetBasePath(Directory.GetCurrentDirectory())
+                          .AddJsonFile("appsettings.json");
+
+            IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+
+            CloudStorageAccount myClient = CloudStorageAccount.Parse(config["connectionstring"]);
+            CloudQueueClient queueClient = myClient.CreateCloudQueueClient();
+        }
+    }
+}

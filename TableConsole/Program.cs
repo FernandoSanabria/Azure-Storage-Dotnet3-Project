84d554fe935bde53e23ea9010f36c1808572fdd4
FernandoSanabria <FernandoSanabria@github.com>
2020-07-04 20:00:16 -0500

    configuracion inicial table storage con errores

---
+++ TableConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Extensions.Configuration;
+
+namespace TableConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                     .SetBasePath(Directory.GetCurrentDirectory())
+                     .AddJsonFile("appsettings.json");
+
+            IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            string getConnString = config["connectionstring"];
+        }
+
+        private static async Task<CloudTable> CreateTableAsync(string tableName, string connectionString)
+        {
+
+        }
+    }
+}
